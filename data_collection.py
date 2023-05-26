e8b2525eb551b2a368da5b659db0880ca876ae64
Miranda Zachopoulou <78528123+mirazach@users.noreply.github.com>
2023-05-26 19:40:52 +0300

    Changing paths to relative

--- data_collection.py
+++ data_collection.py
@@ -1,12 +1,11 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Aug 10 12:18:00 2021
-
 @author: mirandazachopoulou
 """
 
-# %%Importing Libraries 
+# %% Importing Libraries 
 
 import pandas as pd
 import numpy as np
@@ -24,63 +23,59 @@ import GSA
 
 # %% Establishing Spotify connection 
 
-#Setting up authorization manager (link to my Spotify Developer account) 
-auth = SpotifyClientCredentials(client_id = 'client_id', #add client id 
-                                client_secret = 'client_secret') #add client secret
+# Setting up authorization manager (link to my Spotify Developer account) 
+auth = SpotifyClientCredentials(client_id='client_id',  # add client id 
+                                client_secret='client_secret')  # add client secret
 
 sp = spotipy.Spotify(auth_manager=auth)
 
-#Authenticate to access Spotify API through Spotipy
+# Authenticate to access Spotify API through Spotipy
 GSA.authenticate()
 
 # %% Building initial dataset 
 
-#Importing composers dataset 
-composers = pd.read_excel('/Users/mirandazachopoulou/Desktop/BA Report/Composers.xlsx')
+# Importing composers dataset 
+composers = pd.read_excel('Composers.xlsx')
 
-#Compiling information from all Playlists 
+# Compiling information from all Playlists 
 
-#Creating empty dataframe 
-data = pd.DataFrame(columns = ['Composer', 'playlistID', 'TrackName', 'TrackID', 'SampleURL', 'ReleaseYear', 
-                               'Genres', 'danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 
-                               'instrumentalness', 'liveness', 'valence', 'tempo', 'key', 'mode', 'duration_ms', 
-                               'Popularity']) 
+# Creating empty dataframe 
+data = pd.DataFrame(columns=['Composer', 'playlistID', 'TrackName', 'TrackID', 'SampleURL', 'ReleaseYear',
+                             'Genres', 'danceability', 'energy', 'loudness', 'speechiness', 'acousticness',
+                             'instrumentalness', 'liveness', 'valence', 'tempo', 'key', 'mode', 'duration_ms',
+                             'Popularity'])
 
-for i in range(0, len(composers)): 
-    
-    #Track progress 
+for i in range(0, len(composers)):
+
+    # Track progress 
     print(i)
-    
-    #Get composer 
-    composer = composers.iloc[i,0]
-    
-    #Get playlist URI 
-    uri = composers.iloc[i,2]
-    
-    #Obtain plylist information 
+
+    # Get composer 
+    composer = composers.iloc[i, 0]
+
+    # Get playlist URI 
+    uri = composers.iloc[i, 2]
+
+    # Obtain plylist information 
     playlist_info = GSA.getInformation(uri)
-    
-    #Turn into dataframe 
+
+    # Turn into dataframe 
     playlist_info_df = pd.read_pickle(playlist_info)
-   
-    #Combine with composer name 
-    composer_df = pd.DataFrame({'Composer': [composer]*len(playlist_info_df)})
+
+    # Combine with composer name 
+    composer_df = pd.DataFrame({'Composer': [composer] * len(playlist_info_df)})
     playlist_info_df = pd.concat([composer_df, playlist_info_df], axis=1)
-    
-    #Adding to overall dataframe 
+
+    # Adding to overall dataframe 
     data = pd.concat([data, playlist_info_df]).reset_index(drop=True)
-    
+
 
 # %% Import dataframe 
 
-#data.to_excel("/Users/mirandazachopoulou/Desktop/BA Report/Data.xlsx",  index=False) 
- 
-data = pd.read_excel('/Users/mirandazachopoulou/Desktop/BA Report/Data.xlsx')
-
 len(data[data.SampleURL.isnull()])
-#There's 188 missing samples so those rows will be removed  
+# There's 188 missing samples so those rows will be removed  
 
-data.dropna(subset = ["SampleURL"], inplace=True)
+data.dropna(subset=["SampleURL"], inplace=True)
 data = data.reset_index(drop=True)
 
 
@@ -111,7 +106,7 @@ for track in toDownload:
 
 from scipy.io import wavfile
 
-file_names = os.listdir('/Users/mirandazachopoulou/Desktop/BA Report/wav')
+file_names = os.listdir('wav')
 
 # the timestamp to split at (in seconds)
 timestamp_start = 5
@@ -122,8 +117,8 @@ for filename in file_names:
     if filename == '.DS_Store':
         continue 
 
-    # read the file and get the sample rate and data
-    rate, data = wavfile.read('/Users/mirandazachopoulou/Desktop/BA Report/wav/' + filename) 
+    # read the file and get the sample rate and data (create an empty wav folder)
+    rate, data = wavfile.read('wav/' + filename) 
 
     # get the frame to split at
     split_start = rate * timestamp_start
@@ -133,38 +128,38 @@ for filename in file_names:
     sample = data[split_start:split_end-1]  # split
 
     # save the result
-    wavfile.write('/Users/mirandazachopoulou/Desktop/BA Report/wav_red/'+filename, rate, sample)
+    wavfile.write('wav_red/'+filename, rate, sample)
     
  # %% Extracting audio features 
 
 
-#Dataframe column names 
+# Dataframe column names 
 colnames = ['filename', 'chroma_stft', 'spectral_centroid', 'spectral_bandwidth', 'rolloff', 'zero_crossing_rate']
 for i in range(1, 21):
     colnames.append('mfcc'+str(i))
 
-#Creating empty dataframe 
-audio_features = pd.DataFrame(columns = colnames) 
+# Creating empty dataframe 
+audio_features = pd.DataFrame(columns=colnames) 
 
-#Getting filenames in the wav folder 
-file_names = os.listdir('/Users/mirandazachopoulou/Desktop/BA Report/wav_red')
+# Getting filenames in the wav folder 
+file_names = os.listdir('wav_red')
 
 counter = 0 
 
-#Looping through filenames
+# Looping through filenames
 for filename in file_names:
     
     if filename == '.DS_Store':
         continue 
     
-    #to keep track of progress
+    # to keep track of progress
     counter += 1 
     if counter % 10 == 0:
         print(counter)
         
-    y, sr = librosa.load('/Users/mirandazachopoulou/Desktop/BA Report/wav_red/' + filename)
+    y, sr = librosa.load('wav_red/' + filename)
     
-    #Obtaining audio features 
+    # Obtaining audio features 
     chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
     spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
     spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
@@ -176,19 +171,19 @@ for filename in file_names:
         values.append(np.mean(e))
 
     
-    #Adding row to dataframe 
+    # Adding row to dataframe 
     df_length = len(audio_features)
     audio_features.loc[len(audio_features)] = values
     
 # %% Combining with data df
 
-#audi features dataframe back up 
+# audio features dataframe back up 
 audio_features_bu = audio_features.copy()
 
-#Extracting TrackIDs from filenames 
+# Extracting TrackIDs from filenames 
 audio_features['TrackID'] = audio_features.filename.apply(lambda st: st[st.find("ID_")+3:st.find(".wav")])
 
-#Merging 
+# Merging 
 data = pd.merge(data, audio_features, 'left', on='TrackID')
 
 
@@ -197,14 +192,14 @@ data = pd.merge(data, audio_features, 'left', on='TrackID')
 
 data = pd.merge(data, composers[['Last Name', 'Born']], 'left', left_on='Composer', right_on='Last Name')
 
-#Removing irrelevant columns 
+# Removing irrelevant columns 
 data = data.drop(columns=['playlistID', 'SampleURL', 'ReleaseYear', \
                           'speechiness', 'liveness', 'Genres', 'filename', 'Last Name'])
 
-#Removing Albinoni because only 4 pieces came through 
+# Removing Albinoni because only 4 pieces came through 
 data = data[data['Composer']!='Albinoni']
 
-# %%Creating some additional features 
+# %% Creating some additional features 
 
 data['Symphony'] = data["TrackName"].map(lambda x: 1 if "Symphony" in x else 0)
 data['Concerto'] = data["TrackName"].map(lambda x: 1 if "Concerto" in x else 0)
@@ -212,18 +207,5 @@ data['Quartet'] = data["TrackName"].map(lambda x: 1 if "Quartet" in x else 0)
 data['Trio'] = data["TrackName"].map(lambda x: 1 if "Trio" in x else 0)
 data['Sonata'] = data["TrackName"].map(lambda x: 1 if "Sonata" in x else 0)
 
-#Saving as an excel file 
-data.to_excel("/Users/mirandazachopoulou/Desktop/BA Report/Data_Final.xlsx",  index=False) 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Saving as an excel file 
+data.to_excel("Data_Final.xlsx",  index=False)
